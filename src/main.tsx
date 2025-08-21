e137078a184bc87ec90bf9683aa2208ac9e045a6
hirokawa00 <monolab000@gmail.com>
2025-08-21 23:00:47 +0900

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,40 @@
+import { StrictMode } from 'react';
+import './index.css';
+import { createRouter, RouterProvider } from '@tanstack/react-router';
+import ReactDOM from 'react-dom/client';
+import { useAuth } from './hooks/useAuth';
+import { AuthProvider } from './providers/auth-provider';
+
+import { routeTree } from './routeTree.gen';
+
+const router = createRouter({
+  routeTree,
+  context: {
+    // auth will initially be undefined
+    // We'll be passing down the auth state from within a React component
+    auth: undefined!,
+  },
+});
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+function InnerApp() {
+  const auth = useAuth();
+  return <RouterProvider router={router} context={{ auth }} />;
+}
+
+const rootElement = document.getElementById('root')!;
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <StrictMode>
+      <AuthProvider>
+        <InnerApp />
+      </AuthProvider>
+    </StrictMode>,
+  );
+}
---
+++ src/routes/__root.tsx
@@ -0,0 +1,17 @@
+import { createRootRouteWithContext, Outlet } from '@tanstack/react-router';
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
+import type { AuthContextType } from '@/providers/auth-provider';
+
+interface MyRouterContext {
+  // The ReturnType of your useAuth hook or the value of your AuthContext
+  auth: AuthContextType;
+}
+
+export const Route = createRootRouteWithContext<MyRouterContext>()({
+  component: () => (
+    <>
+      <Outlet />
+      <TanStackRouterDevtools />
+    </>
+  ),
+});
